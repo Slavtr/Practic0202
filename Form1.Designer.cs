2d454e74c3a6fe97a655e378a7d05dd6a18d084f
ra5um1st <saintsprizrakk@gmail.com>
2021-05-18 12:43:00 +0300

    Создана основная форма

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,12 +30,203 @@ namespace WindowsFormsApp1
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.guna2BorderlessForm1 = new Guna.UI2.WinForms.Guna2BorderlessForm(this.components);
+            this.guna2Panel1 = new Guna.UI2.WinForms.Guna2Panel();
+            this.guna2GradientButton3 = new Guna.UI2.WinForms.Guna2GradientButton();
+            this.aboutusButton = new Guna.UI2.WinForms.Guna2GradientButton();
+            this.calendarButton = new Guna.UI2.WinForms.Guna2GradientButton();
+            this.workoutButton = new Guna.UI2.WinForms.Guna2GradientButton();
+            this.guna2PictureBox2 = new Guna.UI2.WinForms.Guna2PictureBox();
+            this.guna2ControlBox1 = new Guna.UI2.WinForms.Guna2ControlBox();
+            this.guna2Panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // guna2BorderlessForm1
+            // 
+            this.guna2BorderlessForm1.BorderRadius = 15;
+            this.guna2BorderlessForm1.ContainerControl = this;
+            this.guna2BorderlessForm1.DockIndicatorColor = System.Drawing.Color.Transparent;
+            this.guna2BorderlessForm1.ResizeForm = false;
+            // 
+            // guna2Panel1
+            // 
+            this.guna2Panel1.BackColor = System.Drawing.Color.DimGray;
+            this.guna2Panel1.Controls.Add(this.guna2GradientButton3);
+            this.guna2Panel1.Controls.Add(this.aboutusButton);
+            this.guna2Panel1.Controls.Add(this.calendarButton);
+            this.guna2Panel1.Controls.Add(this.workoutButton);
+            this.guna2Panel1.Controls.Add(this.guna2PictureBox2);
+            this.guna2Panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.guna2Panel1.Location = new System.Drawing.Point(0, 0);
+            this.guna2Panel1.Name = "guna2Panel1";
+            this.guna2Panel1.ShadowDecoration.Parent = this.guna2Panel1;
+            this.guna2Panel1.Size = new System.Drawing.Size(246, 614);
+            this.guna2Panel1.TabIndex = 0;
+            // 
+            // guna2GradientButton3
+            // 
+            this.guna2GradientButton3.Animated = true;
+            this.guna2GradientButton3.CheckedState.Parent = this.guna2GradientButton3;
+            this.guna2GradientButton3.CustomImages.Image = global::WindowsFormsApp1.Properties.Resources.flex_biceps;
+            this.guna2GradientButton3.CustomImages.ImageAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            this.guna2GradientButton3.CustomImages.ImageSize = new System.Drawing.Size(35, 35);
+            this.guna2GradientButton3.CustomImages.Parent = this.guna2GradientButton3;
+            this.guna2GradientButton3.FillColor = System.Drawing.Color.Empty;
+            this.guna2GradientButton3.FillColor2 = System.Drawing.Color.Empty;
+            this.guna2GradientButton3.Font = new System.Drawing.Font("Furore", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.guna2GradientButton3.ForeColor = System.Drawing.Color.White;
+            this.guna2GradientButton3.HoverState.BorderColor = System.Drawing.Color.Gray;
+            this.guna2GradientButton3.HoverState.CustomBorderColor = System.Drawing.Color.Gray;
+            this.guna2GradientButton3.HoverState.FillColor = System.Drawing.Color.Silver;
+            this.guna2GradientButton3.HoverState.FillColor2 = System.Drawing.Color.DimGray;
+            this.guna2GradientButton3.HoverState.Parent = this.guna2GradientButton3;
+            this.guna2GradientButton3.ImageAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            this.guna2GradientButton3.Location = new System.Drawing.Point(0, 329);
+            this.guna2GradientButton3.Name = "guna2GradientButton3";
+            this.guna2GradientButton3.PressedColor = System.Drawing.Color.Gray;
+            this.guna2GradientButton3.ShadowDecoration.Parent = this.guna2GradientButton3;
+            this.guna2GradientButton3.Size = new System.Drawing.Size(246, 45);
+            this.guna2GradientButton3.TabIndex = 5;
+            this.guna2GradientButton3.Text = "...";
+            this.guna2GradientButton3.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            this.guna2GradientButton3.TextOffset = new System.Drawing.Point(60, 0);
+            // 
+            // aboutusButton
+            // 
+            this.aboutusButton.Animated = true;
+            this.aboutusButton.CheckedState.Parent = this.aboutusButton;
+            this.aboutusButton.CustomImages.Image = global::WindowsFormsApp1.Properties.Resources.flex_biceps;
+            this.aboutusButton.CustomImages.ImageAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            this.aboutusButton.CustomImages.ImageSize = new System.Drawing.Size(35, 35);
+            this.aboutusButton.CustomImages.Parent = this.aboutusButton;
+            this.aboutusButton.FillColor = System.Drawing.Color.Empty;
+            this.aboutusButton.FillColor2 = System.Drawing.Color.Empty;
+            this.aboutusButton.Font = new System.Drawing.Font("Furore", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.aboutusButton.ForeColor = System.Drawing.Color.White;
+            this.aboutusButton.HoverState.BorderColor = System.Drawing.Color.Gray;
+            this.aboutusButton.HoverState.CustomBorderColor = System.Drawing.Color.Gray;
+            this.aboutusButton.HoverState.FillColor = System.Drawing.Color.Silver;
+            this.aboutusButton.HoverState.FillColor2 = System.Drawing.Color.DimGray;
+            this.aboutusButton.HoverState.Parent = this.aboutusButton;
+            this.aboutusButton.ImageAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            this.aboutusButton.Location = new System.Drawing.Point(0, 278);
+            this.aboutusButton.Name = "aboutusButton";
+            this.aboutusButton.PressedColor = System.Drawing.Color.Gray;
+            this.aboutusButton.ShadowDecoration.Parent = this.aboutusButton;
+            this.aboutusButton.Size = new System.Drawing.Size(246, 45);
+            this.aboutusButton.TabIndex = 4;
+            this.aboutusButton.Text = "о нас";
+            this.aboutusButton.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            this.aboutusButton.TextOffset = new System.Drawing.Point(60, 0);
+            // 
+            // calendarButton
+            // 
+            this.calendarButton.Animated = true;
+            this.calendarButton.CheckedState.Parent = this.calendarButton;
+            this.calendarButton.CustomImages.Image = global::WindowsFormsApp1.Properties.Resources.calendar__v1;
+            this.calendarButton.CustomImages.ImageAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            this.calendarButton.CustomImages.ImageSize = new System.Drawing.Size(35, 35);
+            this.calendarButton.CustomImages.Parent = this.calendarButton;
+            this.calendarButton.FillColor = System.Drawing.Color.Empty;
+            this.calendarButton.FillColor2 = System.Drawing.Color.Empty;
+            this.calendarButton.Font = new System.Drawing.Font("Furore", 12F, System.Drawing.FontStyle.Italic);
+            this.calendarButton.ForeColor = System.Drawing.Color.White;
+            this.calendarButton.HoverState.BorderColor = System.Drawing.Color.Gray;
+            this.calendarButton.HoverState.CustomBorderColor = System.Drawing.Color.Gray;
+            this.calendarButton.HoverState.FillColor = System.Drawing.Color.Silver;
+            this.calendarButton.HoverState.FillColor2 = System.Drawing.Color.DimGray;
+            this.calendarButton.HoverState.Parent = this.calendarButton;
+            this.calendarButton.ImageAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            this.calendarButton.Location = new System.Drawing.Point(0, 227);
+            this.calendarButton.Name = "calendarButton";
+            this.calendarButton.PressedColor = System.Drawing.Color.Gray;
+            this.calendarButton.ShadowDecoration.Parent = this.calendarButton;
+            this.calendarButton.Size = new System.Drawing.Size(246, 45);
+            this.calendarButton.TabIndex = 3;
+            this.calendarButton.Text = "расписание";
+            this.calendarButton.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            this.calendarButton.TextOffset = new System.Drawing.Point(60, 0);
+            // 
+            // workoutButton
+            // 
+            this.workoutButton.Animated = true;
+            this.workoutButton.CheckedState.Parent = this.workoutButton;
+            this.workoutButton.CustomImages.Image = global::WindowsFormsApp1.Properties.Resources.sportbag;
+            this.workoutButton.CustomImages.ImageAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            this.workoutButton.CustomImages.ImageSize = new System.Drawing.Size(35, 35);
+            this.workoutButton.CustomImages.Parent = this.workoutButton;
+            this.workoutButton.FillColor = System.Drawing.Color.Empty;
+            this.workoutButton.FillColor2 = System.Drawing.Color.Empty;
+            this.workoutButton.Font = new System.Drawing.Font("Furore", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.workoutButton.ForeColor = System.Drawing.Color.White;
+            this.workoutButton.HoverState.FillColor = System.Drawing.Color.Silver;
+            this.workoutButton.HoverState.FillColor2 = System.Drawing.Color.DimGray;
+            this.workoutButton.HoverState.Parent = this.workoutButton;
+            this.workoutButton.ImageAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            this.workoutButton.Location = new System.Drawing.Point(0, 176);
+            this.workoutButton.Name = "workoutButton";
+            this.workoutButton.PressedColor = System.Drawing.Color.Gray;
+            this.workoutButton.ShadowDecoration.Parent = this.workoutButton;
+            this.workoutButton.Size = new System.Drawing.Size(246, 45);
+            this.workoutButton.TabIndex = 2;
+            this.workoutButton.Text = "Тренировка";
+            this.workoutButton.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            this.workoutButton.TextOffset = new System.Drawing.Point(60, 0);
+            this.workoutButton.Click += new System.EventHandler(this.guna2GradientButton1_Click);
+            // 
+            // guna2PictureBox2
+            // 
+            this.guna2PictureBox2.Image = global::WindowsFormsApp1.Properties.Resources.logo_text;
+            this.guna2PictureBox2.Location = new System.Drawing.Point(0, 12);
+            this.guna2PictureBox2.Name = "guna2PictureBox2";
+            this.guna2PictureBox2.ShadowDecoration.Parent = this.guna2PictureBox2;
+            this.guna2PictureBox2.Size = new System.Drawing.Size(256, 115);
+            this.guna2PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.guna2PictureBox2.TabIndex = 1;
+            this.guna2PictureBox2.TabStop = false;
+            // 
+            // guna2ControlBox1
+            // 
+            this.guna2ControlBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.guna2ControlBox1.BackColor = System.Drawing.Color.Transparent;
+            this.guna2ControlBox1.FillColor = System.Drawing.Color.Transparent;
+            this.guna2ControlBox1.HoverState.Parent = this.guna2ControlBox1;
+            this.guna2ControlBox1.IconColor = System.Drawing.Color.White;
+            this.guna2ControlBox1.Location = new System.Drawing.Point(1040, 8);
+            this.guna2ControlBox1.Name = "guna2ControlBox1";
+            this.guna2ControlBox1.ShadowDecoration.Parent = this.guna2ControlBox1;
+            this.guna2ControlBox1.Size = new System.Drawing.Size(31, 29);
+            this.guna2ControlBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.Gray;
+            this.ClientSize = new System.Drawing.Size(1080, 614);
+            this.Controls.Add(this.guna2ControlBox1);
+            this.Controls.Add(this.guna2Panel1);
+            this.ForeColor = System.Drawing.SystemColors.ActiveBorder;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.guna2Panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private Guna.UI2.WinForms.Guna2BorderlessForm guna2BorderlessForm1;
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel1;
+        private Guna.UI2.WinForms.Guna2ControlBox guna2ControlBox1;
+        private Guna.UI2.WinForms.Guna2PictureBox guna2PictureBox2;
+        private Guna.UI2.WinForms.Guna2GradientButton workoutButton;
+        private Guna.UI2.WinForms.Guna2GradientButton guna2GradientButton3;
+        private Guna.UI2.WinForms.Guna2GradientButton aboutusButton;
+        private Guna.UI2.WinForms.Guna2GradientButton calendarButton;
     }
 }
 
